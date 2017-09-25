dd2284f8e1413b5e70ba35d2ba2d56544539a3a9
unknown <ruitiagoribeiroalmeida@gmail.com>
2017-09-25 21:39:25 +0100

    Business Card

---
+++ IP/BusinessCard/BusinessCard/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCard
+{
+    class BusinessCard
+    {
+        static string name = "Rui", phone = "912", occupation = "stud";
+        static void Main(string[] args)
+        {
+            BusinessCard.Print(name, phone, occupation);
+
+            Console.WriteLine("Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("phone:");
+
+            phone = Console.ReadLine();
+            Console.WriteLine("occupation:");
+
+            occupation = Console.ReadLine();
+            BusinessCard.Print(name, phone, occupation);
+            Console.ReadKey();
+
+        }
+        public static void Print(string name, string phone, string occupation)
+        {
+            Console.WriteLine(name + "\n" + phone + "\n" + occupation + "\n\n\n");
+
+        }
+    }
+}
