67eb30ecdc3ac7b1452c4193d181100fa22a92f9
unknown <ruitiagoribeiroalmeida@gmail.com>
2017-09-25 12:56:53 +0100

    Welcome Message

---
+++ IP/WelcomeMessage/WelcomeMessage/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, course;
+            string message;
+
+            Console.WriteLine("Please enter your name");
+            
+            // Here we use readline to pu string data into a variable;
+            name = Console.ReadLine();
+
+            //Ask the course name 
+            Console.WriteLine("Please enter your name");
+            course = Console.ReadLine();
+
+            //Construct a welcome message
+            message = "Welcome... " + name + " To " + "Anglia Ruskin";
+            message += " to the couse " + course;
+            //write the message to the console
+            Console.WriteLine(message);
+            //here we use readline justo to 'pause' the program
+            Console.ReadLine();
+
+        }
+    }
+}
