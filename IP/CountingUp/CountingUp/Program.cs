9e0d10f24c3ffaacc0fbfc0a40906c1f61ea5654
unknown <ruitiagoribeiroalmeida@gmail.com>
2017-09-25 12:48:48 +0100

    Counting up exercise

---
+++ IP/CountingUp/CountingUp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable to store our total number
+            decimal total = 1;
+            Console.WriteLine("Total currently {0}", total);
+            total += 2;
+            Console.WriteLine("Total now: {0}", total);
+            total += 1;
+            Console.WriteLine("Total changed to: {0}", total);
+            //shorthand from of addition (equivalents for */ and -)
+            total += 3;
+            Console.WriteLine("Total finally : {0}", total);
+            //readline to stop cosing before we can read results
+            Console.ReadKey();
+
+        }
+    }
+}
--- IP/HelloWorld/HelloWorld/Program.cs
+++ IP/HelloWorld/HelloWorld/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Author: Rui Almeida
+// SID: 1646818
+// Edited: 25-09-2017
 namespace HelloWorld
 {
     class Program
