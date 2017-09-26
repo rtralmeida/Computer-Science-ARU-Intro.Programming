3f849c9bf66326c90a547b65c2f2ef3ea179df06
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2017-09-26 08:52:08 +0100

    Getting Numbers

---
+++ GettingNumbers/GettingNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable to hold a number
+            decimal numberFromUser = 0m;
+            string userImput = "";
+            
+            Console.WriteLine("please enter a number between 1 and 10. Then press Enter.");
+            userImput = Console.ReadLine();
+
+            // conver imput string into a int
+            numberFromUser = Decimal.Parse(userImput);
+            //add one to the number, prove we can
+            numberFromUser++;
+            Console.WriteLine("Result plus one is :{0}", numberFromUser);
+
+            // Pause
+            Console.ReadLine();
+
+        }
+    }
+}
