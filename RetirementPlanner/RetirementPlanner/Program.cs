063e1256820186a03d836b77de645e3fdd1328df
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2017-09-28 16:10:31 +0100

    Retirement Planner

--- RetirementPlanner/RetirementPlanner/Program.cs
+++ RetirementPlanner/RetirementPlanner/Program.cs
@@ -10,6 +10,28 @@ namespace RetirementPlanner
     {
         static void Main(string[] args)
         {
+            //variable declarations for the input and calculations 
+            string inputAge;
+            decimal age;
+            decimal oldAge = 65;
+            decimal difference;
+            Console.WriteLine("Please enter yout age in whole years");
+            inputAge = Console.ReadLine();
+            //inputAge = Console.ReadLine();
+            //now we need to call a methot to conver from one datatype to another
+            //as we cant subtract a number from a string.
+            //Convert lile console is a library of functions within the System Library
+            //and this is age different type of comment syntax
+            age = Convert.ToDecimal(inputAge);
+            //with these numbers we can do maths
+            difference = oldAge - age;
+            //we cloud have buil the message to show the user ourselves
+            // or we can use this alternative syntax
+            Console.WriteLine("In {0} years you will be 65 years old", difference);
+            //wait for the yser to hit enter
+            Console.ReadLine();
+
+
         }
     }
 }
