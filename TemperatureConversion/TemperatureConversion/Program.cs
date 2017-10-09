5a4adba8e96637eed4d69f2fa62a8acd2ae03547
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2017-10-09 13:03:20 +0100

    Temperature Conversion

---
+++ TemperatureConversion/TemperatureConversion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userAnswer = "";
+            double temp;
+
+
+
+            Console.WriteLine("Program menu: Please type the temprature in Celcius or Fahrenheit or Kelvin");
+            userAnswer = Console.ReadLine();
+
+            temp = Convert.ToDouble(userAnswer);
+
+
+
+
+            Console.WriteLine("{0}º Celcius is {1} Fahrenheit", temp, (temp * 1.8) + 32);
+            Console.WriteLine("{0}º Fahrenheit is {1} Celcius ", temp, (temp - 32) / 1.8);
+            Console.WriteLine("{0}º Fahrenheit is {1} Kelvin", temp, (5 / 9 * (temp - 32) + 273));
+            Console.WriteLine("{0}º Celcius is {1} Kelvin",temp,  temp + 273);
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
