e78b68e138f1b32a678d16720272895b8f897e56
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2017-10-09 13:42:32 +0100

    Bad Data

---
+++ BadData/BadData/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable to hold a number
+            int numberFromUser = 0;
+            string userInput = "";
+
+            Console.WriteLine("please enter a number between 1 and 10. Then press Enter.");
+            userInput = Console.ReadLine();
+
+            // conver imput string into a int
+            numberFromUser = Int32.Parse(userInput);
+
+
+            if (Int32.TryParse(userInput, out numberFromUser))
+            {
+                numberFromUser++;
+                Console.WriteLine("Result plus one is : {0}", numberFromUser);
+            }
+            else
+            {
+                Console.WriteLine("An erro occurred. Did you enter a number?");
+            }
+
+            // Pause
+            Console.ReadLine();
+
+        }
+    }
+}
